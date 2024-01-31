33009cc099337050537bb063f77e80f85888eb08
Big-foot1 <imutyaba11@gmail.com>
2024-01-31 10:27:40 -0800

    tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,6 +12,21 @@ int max(int a, int b)
 	return ((a > b) ? a : b);
 }
 
+/**
+ * binary_tree_is_leaf - function checks if node is  leaf or not
+ * @node: node to be checked
+ * Return: 1 if node if leaf, 0 if not
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
 
 /**
  * binary_tree_height - function computes maximum height of node from root
@@ -24,11 +39,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int right_height;
 	int left_height;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL || binary_tree_is_leaf(tree))
 		return (0);
 	right_height = binary_tree_height(tree->right);
 	left_height = binary_tree_height(tree->left);
-	if (tree->left == NULL && tree->right == NULL)
+	if (binary_tree_is_leaf(tree))
 		return (max(left_height, right_height));
 	return (1 + right_height);
 }
