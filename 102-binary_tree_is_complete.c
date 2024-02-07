19279d12407585ef73aebfc343d499568609634e
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-07 05:48:47 -0800

    check if tree us complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_complete - check if binary tree is complete
+ *@root: pointer to the root node of the tree to check
+ *Return: 1 if tree is complete or 0 otherwise
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	int right, left;
+
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	left = binary_tree_is_complete(tree->left);
+	right = binary_tree_is_complete(tree->right);
+	return (left && right);
+}
